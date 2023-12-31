e4ee880099e6f1d70ba38fbf2c44f6a75e14dbeb
igor-ht <i_ht93@hotmail.com>
2024-01-01 00:00:55 +0200

    geolocation init in process

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,13 @@
 import './Home.scss';
 import CurrentCityWeather from './components/CurrentCityWeather/CurrentCityWeather';
 import SearchCityByName from './components/SearchCityByName/SearchCityByName';
+// import { useDispatch } from 'react-redux';
+// import { accuweatherApi, useGetCityByCoordinatesQuery } from '../../features/api/accuweatherApi';
 
 export default function Home() {
+	// const dispatch = useDispatch();
+	// const { data: geolocation } = useGetCityByCoordinatesQuery(null);
+
 	return (
 		<div className="home-container">
 			<SearchCityByName />
