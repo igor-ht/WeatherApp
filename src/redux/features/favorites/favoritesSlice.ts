0891306ec7a9d06848383aeadd8b0996af2e10ae
igor-ht <i_ht93@hotmail.com>
2024-01-01 14:31:23 +0200

    typo fixed

--- src/redux/features/favorites/favoritesSlice.ts
+++ src/redux/features/favorites/favoritesSlice.ts
@@ -5,34 +5,7 @@ import { CurrentCityType } from '../currentCity/currentCitySlice';
 interface FavoritesI {
 	favorites: CurrentCityType[];
 }
-// [
-// 	{
-// 		city: {
-// 			key: 215854,
-// 			name: 'Netanya',
-// 		},
-// 		currentWeather: {
-// 			WeatherText: 'Partly Cloud',
-// 			Temperature: {
-// 				Metric: { Value: 19.4, Unit: 'C' },
-// 			},
-// 		},
-// 		fiveDaysForecast: [],
-// 	},
-// 	{
-// 		city: {
-// 			key: 215834,
-// 			name: 'Tel Aviv',
-// 		},
-// 		currentWeather: {
-// 			WeatherText: 'Partly Cloud',
-// 			Temperature: {
-// 				Metric: { Value: 19.4, Unit: 'C' },
-// 			},
-// 		},
-// 		fiveDaysForecast: [],
-// 	},
-// ]
+
 const initialState: FavoritesI = {
 	favorites: [],
 };
@@ -43,12 +16,12 @@ const favoritesSlice = createSlice({
 	reducers: {
 		addFavorite: (state, action) => {
 			for (const fav of state.favorites) {
-				if (fav.city.key === action.payload.city.key) return state;
+				if (fav.city?.key === action.payload.city.key) return state;
 			}
 			state.favorites.push(action.payload);
 		},
 		removeFavorite: (state, action) => {
-			return { favorites: state.favorites.filter((favorite) => favorite.city.key !== action.payload.city.key) };
+			return { favorites: state.favorites.filter((favorite) => favorite.city?.key !== action.payload.city.key) };
 		},
 	},
 });
